bbe2911b19a51ef10acaf53fd2c284c436b1d514
Tim Harrison <heedspin@gmail.com>
2011-08-24 14:15:24 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'rails', '2.3.14'
+gem 'mysql', '2.8.1'
+gem 'tiny_tds'
+gem 'activerecord-sqlserver-adapter', '2.3.20'
+
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'a31e7cce92a89d7b52b3dcd094697c2c8afca61b4b822d9d527d6917579226404f8c439eaedff8147b05de4493d11862f0304541b4e43249caeeb26399337ccb';
