0a5454ad94bf17641dfe02c4b8344baa9163b472
Tim Harrison <heedspin@gmail.com>
2011-09-11 14:42:46 -0400

    Try to fix password reset on production.

--- app/models/password_reset_notifier.rb
+++ app/models/password_reset_notifier.rb
@@ -13,7 +13,7 @@ class PasswordResetNotifier < ActionMailer::Base
     from       CompanyConfig.email_address
     recipients user.email
     sent_on    Time.now
-    body       :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
+    body       :edit_password_reset_url => edit_password_reset_url(user.perishable_token, :host => CompanyConfig.hostname)
 
   end
 
