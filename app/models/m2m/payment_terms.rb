cf068f75d21a1f5f67d3e825e9080dbee5f9ac0f
Tim Harrison <heedspin@gmail.com>
2011-09-12 14:37:30 -0400

    Still trying to understand production error.

--- app/models/m2m/payment_terms.rb
+++ app/models/m2m/payment_terms.rb
@@ -1,9 +1,7 @@
-require 'active_hash_methods'
-class M2m::Terms < ActiveHash::Base
+class M2m::PaymentTerms < ActiveHash::Base
   self.data = [
     {:key => 'A', :name => 'Net 30'},
     {:key => 'F', :name => '2% 10, Net 30'},
     {:key => 'I', :name => 'Net 60'},
   ]
-  # include ActiveHashMethods
 end
