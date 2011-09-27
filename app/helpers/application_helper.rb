88f0d20cb24c7795a42b541ac8fa08e2d13faab7
Tim Harrison <heedspin@gmail.com>
2011-09-26 21:04:16 -0400

    Override item_url (in views) to allow slashes in part numbers to not poo the bed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def item_url(part_number)
+    super part_number.strip.gsub('/','%2F')
+  end
 end
