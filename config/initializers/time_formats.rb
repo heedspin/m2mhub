117b53a4b76b8f30f93f11aba18c74f282eb0fca
Tim Harrison <heedspin@gmail.com>
2011-10-22 11:02:49 -0400

    Fix the shipper navigation links to use correct database date formatting.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -4,6 +4,7 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:purchase_date] =
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:day_month_year] = '%d-%m-%y'
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:shipper_date] = '%m-%d-%y'
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%y'
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:database] = '%Y-%m-%d'
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:number_date] = '%m-%d-%y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:sales_date] = '%m-%d-%y'
@@ -14,3 +15,4 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:event_date] = '%
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:day_month_year] = '%d-%m-%y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:shipper_date] = '%m-%d-%y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%y'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:database] = '%Y-%m-%d'
