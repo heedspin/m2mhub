d2cc65d7c6b5b3df712fa140d8dfa5a613c15847
Tim Harrison <heedspin@gmail.com>
2011-10-22 11:07:41 -0400

    Fix backorder report date formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -3,7 +3,7 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:sales_date] = '%
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:purchase_date] = '%m-%d-%y'
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:day_month_year] = '%d-%m-%y'
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:shipper_date] = '%m-%d-%y'
-ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%y'
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%Y'
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:database] = '%Y-%m-%d'
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:number_date] = '%m-%d-%y'
@@ -14,5 +14,5 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:time] = '%I:%M %
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:event_date] = '%b %d, %Y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:day_month_year] = '%d-%m-%y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:shipper_date] = '%m-%d-%y'
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%y'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%Y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:database] = '%Y-%m-%d'
