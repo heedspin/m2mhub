45d5de8aa9257827c05740a7278846c327f7048a
Tim Harrison <heedspin@gmail.com>
2011-12-14 19:17:08 -0500

    Add time format for customer otd script runner.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -6,6 +6,7 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:day_month_year] 
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:shipper_date] = '%m-%d-%y'
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%Y'
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:database] = '%Y-%m-%d'
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:month_year] = '%b %Y'
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:number_date] = '%m-%d-%y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:job_date] = '%m-%d-%y'
@@ -18,3 +19,4 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:day_month_year] 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:shipper_date] = '%m-%d-%y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:backorder_date] = '%m/%d/%Y'
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:database] = '%Y-%m-%d'
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:month_year] = '%b %Y'
