7c3ac30f399d211e69c2f238cc334cb1c3878e89
Tim Harrison <heedspin@gmail.com>
2011-12-14 19:12:27 -0500

    Add customer otd report and script runner.

--- app/models/m2m/constants.rb
+++ app/models/m2m/constants.rb
@@ -1,6 +1,8 @@
 class M2m::Constants
   @@null_date = Date.parse('1900-01-01')
   cattr_accessor :null_date
+  @@null_time = Time.parse('1900-01-01')
+  cattr_accessor :null_time
   
   def self.sanitize_date(date)
     date == M2m::Constants.null_date ? nil : date
