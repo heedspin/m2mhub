82fc008ce176077526cb5041ac72c25763a3e4a6
Tim Harrison <heedspin@gmail.com>
2011-12-19 15:40:04 -0500

    Try to patch url creation for quality namespace.

--- app/helpers/user_activities_helper.rb
+++ app/helpers/user_activities_helper.rb
@@ -1,6 +1,10 @@
 module UserActivitiesHelper
   def user_activity_url(user_activity)
     begin
+      cs = user_activity.params_hash['controller']
+      if ['customer_otd_reports', 'rma_reports', 'credit_memo_reports'].include?(cs)
+        user_activity.params_hash['controller'] = 'quality/' + cs
+      end
       url_for user_activity.params_hash
     rescue ActionView::TemplateError
       user_activity.report_name
