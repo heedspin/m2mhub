a30c31808969358cd59b3f27650b3067a0f801fd
Tim Harrison <heedspin@gmail.com>
2011-12-19 16:21:47 -0500

    Still trying to fix routing errors.

--- app/helpers/user_activities_helper.rb
+++ app/helpers/user_activities_helper.rb
@@ -1,11 +1,12 @@
 module UserActivitiesHelper
   def user_activity_url(user_activity)
     begin
-      cs = user_activity.params_hash['controller']
+      phash = user_activity.params_hash
+      cs = phash['controller']
       if ['customer_otd_reports', 'rma_reports', 'credit_memo_reports'].include?(cs)
-        user_activity.params_hash['controller'] = 'quality/' + cs
+        phash['controller'] = 'quality/' + cs
       end
-      url_for user_activity.params_hash
+      url_for phash
     rescue ActionView::TemplateError
       user_activity.report_name
     end
