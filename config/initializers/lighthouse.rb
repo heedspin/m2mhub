02ce9c9f714148a5f91458f7e0a9146147d64604
Tim Harrison <heedspin@gmail.com>
2012-01-16 08:17:05 -0500

    Ignore lighthouse unless it's configured.

--- config/initializers/lighthouse.rb
+++ config/initializers/lighthouse.rb
@@ -1,2 +1,4 @@
-Lighthouse.account = CompanyConfig.lighthouse_account
-Lighthouse.token = CompanyConfig.lighthouse_token
+if CompanyConfig.lighthouse_account
+  Lighthouse.account = CompanyConfig.lighthouse_account
+  Lighthouse.token = CompanyConfig.lighthouse_token
+end
