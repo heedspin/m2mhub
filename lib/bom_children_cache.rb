0160ccb8341cbfe7903f2687f8b829b944acb520
Tim Harrison <heedspin@gmail.com>
2012-03-31 22:38:38 -0400

    Fix horrid bom children cache bug.

--- lib/bom_children_cache.rb
+++ lib/bom_children_cache.rb
@@ -8,23 +8,24 @@ class BomChildrenCache
     misses = []
     caches = []
     # Add to result from cache
-    releases = releases.delete_if do |release|
+    releases_to_load = []
+    releases.each do |release|
       item_key = [release.part_number, release.revision]
       release.instance_variable_set :@item_key, [release.part_number, release.revision]
       if children = @cache[item_key]
         result[release] = children
         hits.push item_key
-        true
       else
+        releases_to_load.push release
         misses.push item_key
         false
       end
     end
     bom_items = {}
-    M2m::BomItem.with_parent_part_numbers(releases.map(&:part_number).uniq).each do |bom_item|
-      (bom_items[[bom_item.part_number, bom_item.revision]] ||= []).push(bom_item)
+    M2m::BomItem.with_parent_part_numbers(releases_to_load.map(&:part_number).uniq).each do |bom_item|
+      (bom_items[[bom_item.parent_part_number, bom_item.parent_revision]] ||= []).push(bom_item)
     end
-    releases.each do |release|
+    releases_to_load.each do |release|
       item_key = release.instance_variable_get :@item_key
       @cache[item_key] = result[release] = (bom_items[item_key] || [])
       caches.push item_key
