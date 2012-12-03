99ac97476d6a7a117f457591c91238de9892a1e8
Tim Harrison <heedspin@gmail.com>
2012-12-03 15:50:28 -0500

    Allow feature to enable all.

--- app/models/m2mhub/feature.rb
+++ app/models/m2mhub/feature.rb
@@ -1,20 +1,30 @@
 class M2mhub::Feature
+  class Config
+    attr_accessor :name
+    def initialize(name)
+      @name = name
+      config_string = AppConfig.get("feature_#{@name}")
+      @config = if(config_string == 'all')
+        'all'
+      else
+        Set.new config_string.split(',').map(&:strip).compact
+      end
+    end
+    
+    def enabled?(user)
+      (@config == 'all') || @config.member?(user.email)
+    end
+  end
+  
   class << self
     attr_accessor :current_user
     def enabled?(feature)
-      current_user.present? && self.users_for_feature(feature).member?(current_user.email)
+      current_user.present? && self.feature_config(feature).try(:enabled?, current_user)
     end
     
-    def users_for_feature(feature)
-      @users_for_feature ||= Hash.new
-      if (users = @users_for_feature[feature]).nil?
-        users = Set.new
-        if email_string = AppConfig.get("feature_#{feature}")
-          users = email_string.split(',').map(&:strip).compact
-        end
-        @users_for_feature[feature] = users
-      end
-      users
+    def feature_config(feature)
+      @feature_configs ||= Hash.new
+      @feature_configs[feature] ||= M2mhub::Feature::Config.new(feature)
     end
   end
 end
--- lib/m2mhub/version.rb
+++ lib/m2mhub/version.rb
@@ -1,3 +1,3 @@
 module M2mhub
-  VERSION = '4.12.0'
+  VERSION = '4.12.1'
 end
