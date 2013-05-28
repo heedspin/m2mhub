b5689238250783dfc63cff795702d75d5e0bfeed
Tim Harrison <heedspin@gmail.com>
2013-05-28 10:09:51 -0400

    Set opportunity owner to current user.

--- app/controllers/sales/opportunities_controller.rb
+++ app/controllers/sales/opportunities_controller.rb
@@ -43,6 +43,7 @@ class Sales::OpportunitiesController < M2mhubController
 
   def new
     @opportunity = build_object
+    @opportunity.owner_id = current_user.id
     if @opportunity.sales_customer
       @opportunity.customer_name = @opportunity.sales_customer.name
     else
--- lib/m2mhub/version.rb
+++ lib/m2mhub/version.rb
@@ -1,3 +1,3 @@
 module M2mhub
-  VERSION = '8.8.4'
+  VERSION = '8.9.0'
 end
