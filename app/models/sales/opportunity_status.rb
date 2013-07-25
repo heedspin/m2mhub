420c77a098c72a08c5405530b144ca24db3669c9
Tim Harrison <heedspin@gmail.com>
2013-07-25 13:23:44 -0400

    Only show assignable opportunity statae. Remove quote field from comments.

--- app/models/sales/opportunity_status.rb
+++ app/models/sales/opportunity_status.rb
@@ -1,7 +1,7 @@
 require 'plutolib/active_hash_methods'
 class Sales::OpportunityStatus < ActiveHash::Base
   self.data = [
-    {:id => 8, :open => false, :name => 'All', :_children_ids => [1, 2, 3, 4, 7]},
+    {:id => 8, :open => false, :name => 'All', :cmethod => :is_all, :_children_ids => [1, 2, 3, 4, 7]},
     {:id => 9, :open => false, :name => 'Open and Hold', :_children_ids => [1, 2, 3]},
     {:id => 1, :open => true,  :name => 'Open', :cmethod => :active},
     {:id => 2, :open => false, :name => 'Won'},
@@ -14,8 +14,8 @@ class Sales::OpportunityStatus < ActiveHash::Base
     self._children_ids || [ self.id ]
   end
   class << self
-    def all_not_deleted
-      @all_not_deleted ||= all.select { |s| !s.deleted? }
+    def all_assignable
+      @all_assignable ||= all.select { |s| !s.deleted? && !s.is_all? && !s.open_and_hold? }
     end
     def all_open
       @all_open ||= all.select(&:open?)
--- lib/m2mhub/version.rb
+++ lib/m2mhub/version.rb
@@ -1,3 +1,3 @@
 module M2mhub
-  VERSION = '12.2.4'
+  VERSION = '12.3.1'
 end
