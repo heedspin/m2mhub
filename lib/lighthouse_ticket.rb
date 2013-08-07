1ba5bf5772db9398e6d240bd2620ee2df5236c1f
Tim Harrison <heedspin@gmail.com>
2013-08-07 16:13:36 -0400

    Protect against case when lighthouse isn't installed.

--- lib/lighthouse_ticket.rb
+++ lib/lighthouse_ticket.rb
@@ -1,58 +1,60 @@
-class Lighthouse::Ticket
-  def title
-    attributes['title'] ||= ''
-  end
+module Lighthouse
+  class Ticket
+    def title
+      attributes['title'] ||= ''
+    end
 
-  def title=(value)
-    attributes['title'] = value
-  end
+    def title=(value)
+      attributes['title'] = value
+    end
 
-  def assigned_user_id
-    attributes['assigned_user_id'] ||= ''
-  end
+    def assigned_user_id
+      attributes['assigned_user_id'] ||= ''
+    end
 
-  def assigned_user_id=(value)
-    attributes['assigned_user_id'] = value
-  end
-  
-  def assigned_user_name
-    attributes['assigned_user_name']
-  end
-  
-  def watcher_ids=(value)
-    attributes['multiple_watchers'] = value
-  end
-  
-  def milestone_title
-    attributes['milestone_title']
-  end
-  
-  STATE_INVALID = 'invalid'
-  
-  def closed?
-    self.closed
-    # ['resolved', STATE_INVALID].include?(self.state)
-  end
-  
-  def hold?
-    self.state == 'hold'
-  end
-  
-  def resolved?
-    self.state == 'resolved'
-  end
-  
-  def versions
-    attributes['versions'] || []
-  end
-  
-  ######################################
-  # Trick formtastic:
-  def new_record?
-    false
-  end
-  
-  def to_s
-    "Ticket ##{self.id} #{self.title}"
+    def assigned_user_id=(value)
+      attributes['assigned_user_id'] = value
+    end
+    
+    def assigned_user_name
+      attributes['assigned_user_name']
+    end
+    
+    def watcher_ids=(value)
+      attributes['multiple_watchers'] = value
+    end
+    
+    def milestone_title
+      attributes['milestone_title']
+    end
+    
+    STATE_INVALID = 'invalid'
+    
+    def closed?
+      self.closed
+      # ['resolved', STATE_INVALID].include?(self.state)
+    end
+    
+    def hold?
+      self.state == 'hold'
+    end
+    
+    def resolved?
+      self.state == 'resolved'
+    end
+    
+    def versions
+      attributes['versions'] || []
+    end
+    
+    ######################################
+    # Trick formtastic:
+    def new_record?
+      false
+    end
+    
+    def to_s
+      "Ticket ##{self.id} #{self.title}"
+    end
   end
 end
