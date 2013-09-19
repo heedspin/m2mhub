b5f00d1f05fa7ba2b71abbd87e8c8c6753837cb6
Tim Harrison <heedspin@gmail.com>
2013-09-19 17:50:58 -0400

    Include complete jobs in "released" group.

--- app/models/shipping/include_jobs.rb
+++ app/models/shipping/include_jobs.rb
@@ -2,7 +2,7 @@ require 'plutolib/active_hash_methods'
 class Shipping::IncludeJobs < ActiveHash::Base
   self.data = [
     {:id => 1, :name => 'No Jobs', :cmethod => 'none', :job_status => []},
-    {:id => 2, :name => 'Released Jobs', :cmethod => 'released', :job_status => [M2m::Status.released.name]}, 
+    {:id => 2, :name => 'Released Jobs', :cmethod => 'released', :job_status => [M2m::Status.released.name, M2m::Status.completed.name]}, 
     {:id => 3, :name => 'Open & Released', :cmethod => 'open', :job_status => [M2m::Status.open.name, M2m::Status.released.name, M2m::Status.completed.name]} 
   ]
   include Plutolib::ActiveHashMethods
