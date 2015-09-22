5badaac753fc8570e64ce9f18803b8dd67517577
Tim Harrison <heedspin@gmail.com>
2015-09-22 14:38:22 -0400

    Make nil safe.

--- app/models/quality/customer_otd_report.rb
+++ app/models/quality/customer_otd_report.rb
@@ -30,8 +30,9 @@ class Quality::CustomerOtdReport
     end
     def add_late_release(release)
       @late_releases.push release
-      vendor_name = release.item.vendors.first.vendor.name
-      (@vendor_lates[vendor_name] ||= VendorLates.new(vendor_name)).add_late_release(release)
+      if vendor_name = release.item.vendors.first.try(:vendor).try(:name)
+        (@vendor_lates[vendor_name] ||= VendorLates.new(vendor_name)).add_late_release(release)
+      end
     end
     def num_late_releases
       @late_releases.size
@@ -96,8 +97,10 @@ class Quality::CustomerOtdReport
     late_releases = M2m::SalesOrderRelease.shipped_late.due(@start_date, @end_date)
     M2m::SalesOrderItem.attach_to_releases(late_releases)
     late_releases.each do |release|
-      month_for(release.due_date).add_late_release(release)
-      self.get_vendor_late(release.item.vendors.first.vendor.name).add_late_release(release)
+      if vendor_name = release.item.vendors.first.try(:vendor).try(:name)
+        month_for(release.due_date).add_late_release(release)
+        self.get_vendor_late(vendor_name).add_late_release(release)
+      end
     end
     true
   end
--- lib/m2mhub/version.rb
+++ lib/m2mhub/version.rb
@@ -1,3 +1,3 @@
 module M2mhub
-  VERSION = '13.19.2'
+  VERSION = '13.19.3'
 end
