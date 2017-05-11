4d2c1a143dbc60f0ad1bae2ecd3498a091f3bd9b
Tim Harrison <heedspin@gmail.com>
2017-05-11 11:05:13 -0400

    Only update ticket if quantity > 0

--- app/models/quality/base_inspection_runner.rb
+++ app/models/quality/base_inspection_runner.rb
@@ -6,13 +6,15 @@ class Quality::BaseInspectionRunner
   def handle_receipt(task, receipt)
     begin
       if task.last_receiver.nil? or (task.last_receiver.time_received < receipt.time_received)
-        log "New receipt"
         task.last_receiver_id = receipt.id
         task.status = Quality::InspectionTaskStatus.in_inspection
         quantity = receipt.items.select { |i| i.part_number == task.part_number }.sum(&:quantity)
-        task.update_ticket "Received #{comma(quantity)} #{task.part_number} at #{receipt.time_received.to_s(:short_human_date_time)}\nReceiver: #{receipt.receiver_number}"
-        task.lighthouse_watcher_update
-        task.save!
+        if quantity > 0
+          log "New receipt"
+          task.update_ticket "Received #{comma(quantity)} #{task.part_number} at #{receipt.time_received.to_s(:short_human_date_time)}\nReceiver: #{receipt.receiver_number}"
+          task.lighthouse_watcher_update
+          task.save!
+        end
       else
         log "Old receipt"
       end
