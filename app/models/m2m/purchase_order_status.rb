ee6985076730c6a1595ceb90871acaa757ff92d4
Tim Harrison <heedspin@gmail.com>
2017-09-21 15:12:46 -0400

    Add hold status and char values.

--- app/models/m2m/purchase_order_status.rb
+++ app/models/m2m/purchase_order_status.rb
@@ -1,10 +1,11 @@
 require 'plutolib/active_hash_methods'
 class M2m::PurchaseOrderStatus < ActiveHash::Base
   self.data = [
-    {:id => 1, :m2mname => 'OPEN',      :name => 'Open'},
-    {:id => 2, :m2mname => 'CLOSED',    :name => 'Closed'},
-    {:id => 3, :m2mname => 'CANCELLED', :name => 'Cancelled'},
-    {:id => 4, :m2mname => 'STARTED',   :name => 'Started'}
+    {:id => 1, :m2mname => 'OPEN',      :char => 'O', :name => 'Open'},
+    {:id => 2, :m2mname => 'CLOSED',    :char => 'C', :name => 'Closed'},
+    {:id => 3, :m2mname => 'CANCELLED', :char => 'X', :name => 'Cancelled'},
+    {:id => 4, :m2mname => 'STARTED',   :char => 'S', :name => 'Started'},
+    {:id => 5, :m2mname => 'HOLD',      :char => 'H', :name => 'Hold'}
   ]
   include Plutolib::ActiveHashMethods
 end
