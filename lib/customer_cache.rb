a227573dff34e82ea5857022348592610e385ceb
Tim Harrison <heedspin@gmail.com>
2018-10-30 15:29:22 -0400

    Convert customer id to integer

--- lib/customer_cache.rb
+++ lib/customer_cache.rb
@@ -11,7 +11,7 @@ class CustomerCache
     if @sales_customers_by_erp_customer_id.nil?      
       @sales_customers_by_erp_customer_id = {}
       Sales::Customer.where('erp_customer_id is not null').each do |sales_customer|
-        @sales_customers_by_erp_customer_id[sales_customer.erp_customer_id] = sales_customer
+        @sales_customers_by_erp_customer_id[sales_customer.erp_customer_id.to_i] = sales_customer
       end
     end
     @sales_customers_by_erp_customer_id
