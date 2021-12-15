735abf4ab038974dc5a37cf7ba5b748e39975901
Tim Harrison <heedspin@gmail.com>
2021-12-15 09:05:40 -0500

    Bye

--- config/initializers/lighthouse.rb
+++
@@ -1,7 +0,0 @@
-if AppConfig.lighthouse_account
-  Lighthouse.account = AppConfig.lighthouse_account
-  Lighthouse.token = AppConfig.lighthouse_token
-  require 'lighthouse_project'
-  require 'lighthouse_ticket'
-  require 'lighthouse_user'
-end
